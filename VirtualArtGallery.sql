5636c7876c699ef2e637083aff407c495bb9ffde
Arpit Vaksh <109533304+Arpit-Vaksh@users.noreply.github.com>
2024-09-25 01:50:02 +0530

    Update VirtualArtGallery.sql

--- VirtualArtGallery.sql
+++ VirtualArtGallery.sql
@@ -1,3 +1,66 @@
+CREATE DATABASE virtual_art_gallery;
+
 USE virtual_art_gallery;
+
+CREATE TABLE Artist (
+    ArtistID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(50) NOT NULL,
+    Biography TEXT,
+    BirthDate DATE,
+    Nationality VARCHAR(50),
+    Website VARCHAR(100),
+    ContactInfo VARCHAR(200)
+);
+
+CREATE TABLE Artwork (
+    ArtworkID INT PRIMARY KEY AUTO_INCREMENT,
+    Title VARCHAR(100) NOT NULL,
+    Description TEXT,
+    CreationDate DATE,
+    Medium VARCHAR(100),
+    ImageURL VARCHAR(200),
+    ArtistID INT,
+    FOREIGN KEY (ArtistID) REFERENCES Artist(ArtistID)
+);
+
+CREATE TABLE User (
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    Username VARCHAR(50) NOT NULL UNIQUE,
+    Password VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) NOT NULL UNIQUE,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    DateOfBirth DATE,
+    ProfilePicture VARCHAR(255)
+);
+
+CREATE TABLE Gallery (
+    GalleryID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(100) NOT NULL,
+    Description TEXT,
+    Location VARCHAR(200),
+    Curator INT,
+    OpeningHours VARCHAR(100),
+    FOREIGN KEY (Curator) REFERENCES Artist(ArtistID)
+);
+
+-- Tables for Many to Many relationship 
+
+CREATE TABLE User_Favorite_Artwork (
+    UserID INT,
+    ArtworkID INT,
+    PRIMARY KEY (UserID, ArtworkID),
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (ArtworkID) REFERENCES Artwork(ArtworkID)
+);
+
+CREATE TABLE Artwork_Gallery (
+    ArtworkID INT,
+    GalleryID INT,
+    PRIMARY KEY (ArtworkID, GalleryID),
+    FOREIGN KEY (ArtworkID) REFERENCES Artwork(ArtworkID),
+    FOREIGN KEY (GalleryID) REFERENCES Gallery(GalleryID)
+);
+
 
 
